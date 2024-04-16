1045ee586a81c964cb83cbf2571378d9949699a3
Jorge Callejo Martinez <jcallejo@c3r1s5.42malaga.com>
2024-04-16 10:16:17 +0200

    Arreglado map y comprobado todo

--- Libft/ft_lstmap_bonus.c
+++ Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jcallejo <jcallejo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 21:28:17 by jcallejo          #+#    #+#             */
-/*   Updated: 2024/04/16 09:15:14 by jcallejo         ###   ########.fr       */
+/*   Updated: 2024/04/16 10:09:48 by jcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*map;
 	t_list	*aux;
+	void	*temp;
 
 	if (!lst)
 		return (NULL);
 	map = NULL;
+	temp = map;
 	while (lst)
 	{
-		aux = ft_lstnew(f(lst-> content));
+		temp = f(lst-> content);
+		aux = ft_lstnew(temp);
 		if (!aux)
 		{
+			del(temp);
 			ft_lstclear(&map, del);
 			return (NULL);
 		}
