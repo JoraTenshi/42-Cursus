11c3b4bbab46376e28ffc18e08909e627d24d98c
Jorge Callejo Martinez <jcallejo@c3r1s5.42malaga.com>
2024-04-25 10:21:23 +0200

    Añadida docu a printf

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcallejo <jcallejo@student.42.fr>>         +#+  +:+       +#+        */
+/*   By: jcallejo <jcallejo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:40:45 by jcallejo          #+#    #+#             */
-/*   Updated: 2024/04/23 11:41:35 by jcallejo         ###   ########.fr       */
+/*   Updated: 2024/04/24 10:51:14 by jcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 
