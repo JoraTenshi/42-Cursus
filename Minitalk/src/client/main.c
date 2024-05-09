a97c30978efca219a3a7425a7fd1b3a1fd4c5ed7
JoraTenshi <jorgecmgi2c@gmail.com>
2024-05-09 14:25:22 +0200

    Hecho main de client y migrado libft

--- Minitalk/src/client/main.c
+++ Minitalk/src/client/main.c
@@ -6,8 +6,28 @@
 /*   By: jcallejo <jcallejo@student.42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:21:59 by jcallejo          #+#    #+#             */
-/*   Updated: 2024/05/09 11:24:09 by jcallejo         ###   ########.fr       */
+/*   Updated: 2024/05/09 14:24:41 by jcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/inc/libft/libft.h"
+
+void	bit_send(int pid, char c)
+{
+	
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (*argv[2])
+			byte_send(pid, *argv[2]++);
+	}
+	else
+		ft_printf("You didn't introduce a valid argument");
+	return (0);
+}
