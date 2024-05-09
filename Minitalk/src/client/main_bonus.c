fc56476861a9c8a77bf17bc87145e0f0099e834d
JoraTenshi <jorgecmgi2c@gmail.com>
2024-05-09 18:58:45 +0200

    Minitalk terminado

--- Minitalk/src/client/main_bonus.c
+++ Minitalk/src/client/main_bonus.c
@@ -1,17 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcallejo <jcallejo@student.42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:21:59 by jcallejo          #+#    #+#             */
-/*   Updated: 2024/05/09 18:54:03 by jcallejo         ###   ########.fr       */
+/*   Updated: 2024/05/09 18:57:16 by jcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/inc/libft/libft.h"
 #include <signal.h>
+
+void	ft_msg(int signal)
+{
+	if (signal)
+		ft_print("Message received\n");
+}
 
 void	ft_bit_send(int pid, char c)
 {
@@ -36,6 +42,7 @@ int	main(int argc, char **argv)
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
+		signal(SIGUSR1, ft_msg);
 		while (*argv[2])
 			ft_bit_send(pid, *argv[2]++);
 		ft_bit_send(pid, 0);
