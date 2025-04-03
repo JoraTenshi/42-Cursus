0f95a61735f13202522828eb61d89e0786f2a23e
jcallejo <jcallejo@student.42malaga.com>
2025-04-03 15:16:45 +0200

    minor fix and added main conditions in ex03 of Cpp04

--- Cpp/Cpp04/ex03/src/main.cpp
+++ Cpp/Cpp04/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcallejo <jcallejo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 13:49:20 by jcallejo          #+#    #+#             */
-/*   Updated: 2025/04/03 13:53:10 by jcallejo         ###   ########.fr       */
+/*   Updated: 2025/04/03 15:16:12 by jcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,30 @@ int main()
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Ice()); // materia source full
 
 	ICharacter* noctis = new Character("Noctis Lucis Caelum");
-	
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	noctis->equip(tmp);
 	tmp = src->createMateria("cure");
 	noctis->equip(tmp);
-
+	tmp = src->createMateria("ice");
+	noctis->equip(tmp);
+	tmp = src->createMateria("cure");
+	noctis->equip(tmp);
+	tmp = src->createMateria("ice"); //inventory full
+	noctis->equip(tmp);
+	noctis->unequip(2);
+	
 	ICharacter* shtola = new Character("Y'shtola Rhul");
-
 	noctis->use(0, *shtola);
 	noctis->use(1, *shtola);
+	noctis->use(2, *shtola);
+
+	
 
 	delete noctis;
 	delete shtola;
