1402870c7e6f27cc43769aaf960e29c59df7d675
root <root@jcallejo.42malaga.com>
2025-05-22 09:19:27 +0000

    Nginx done except a makefile error in volume

--- inception/srcs/requirements/nginx/tools/config.sh
+++ inception/srcs/requirements/nginx/tools/config.sh
@@ -11,3 +11,15 @@ else
 fi
 
 
+	if [ ! -f /etc/nginx/http.d/$DOMAIN_NAME.conf ]; then
+		echo -e "\033[1;32mNginx configuration file not found, creating one for $DOMAIN_NAME\033[0m"
+		envsubst '$DOMAIN_NAME' < /nginx_base.conf > /etc/nginx/http.d/$DOMAIN_NAME.conf
+	
+	else
+		echo -e "\033[1;32Using existing nginx configuration file for $DOMAIN_NAME\033[0m"
+	
+	fi
+
+
+	echo -e "\033[1;32mStarting nginx[0m"
+	nginx -g "daemon off;"
